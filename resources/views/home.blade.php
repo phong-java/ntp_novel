79b4843fb3e2377d4f8f5a421f7c381643e1dd15
PhongPhamTuan <mochimochipo1122@gmail.com>
2024-06-01 23:28:19 +0700

    phong

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    Bạn đã đăng nhập thành công
                 </div>
             </div>
         </div>
