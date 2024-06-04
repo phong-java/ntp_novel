a5c8db1fc069e79f11b70534a8edb8ce4c995861
PhongPhamTuan <mochimochipo1122@gmail.com>
2024-06-04 21:03:16 +0700

    phong

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,21 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<div class="row">
+    <div class="col-md-11 container py-4 ">
+        <div class="card">
+            <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    Bạn đã đăng nhập thành công
+            <div class="card-body">
+                @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
                 </div>
+                @endif
+
+                Bạn đã đăng nhập thành công
             </div>
         </div>
     </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,9 +8,14 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
+        
+        <script src="{{ asset('js/app.js') }}" ></script>
+        <script src="{{ asset('js/jquery.js') }}" ></script>
        
     </head>
     <body class="antialiased">
-
+        <input id="phog" type="text">
+        <a class=" mobile-search" href="javascript:void(0);">okokokok</a>
     </body>
 </html>
