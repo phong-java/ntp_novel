bb67a396eb0cdf32b05ac57a683feb694821d76d
PhongPhamTuan <mochimochipo1122@gmail.com>
2024-06-06 18:44:49 +0700

    phong

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,10 @@
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
+        <script src="{{ asset('js/app.js') }}" defer></script>
+        <script src="{{ asset('js/jquery.js') }}" defer></script>
+        <script src="{{ asset('js/main.js') }}" defer></script>
+
         <!-- Styles -->
     </head>
     <body class="antialiased">
