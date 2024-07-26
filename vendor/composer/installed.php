08b2556f56be9799d0942595c0d5ca9f8c484e24
PhongPhamTuan <mochimochipo1122@gmail.com>
2024-07-26 13:03:50 +0700

    phong

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0013550a9270ef2bca03039a742901bcf5f12898',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -404,9 +404,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0013550a9270ef2bca03039a742901bcf5f12898',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
